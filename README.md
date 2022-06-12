f2595be3a401e522464929f71a50b3c756cb662a
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2022-06-12 23:23:56 +0900

    add: typical90,4,10

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-# abc
-## リポジトリ概要
-競技プログラミング用リポジトリ
-
-## フォルダ
-- `abc/src/selected_past_questions`
-  - https://qiita.com/drken/items/fd4e5e3630d0f5859067
+# ABC
+競技プログラミング勉強用
---
+++ typical90/no_004_cross_cum/src/main.rs
@@ -0,0 +1,27 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        h: usize,
+        w: usize,
+        a: [[u64; w]; h],
+    }
+
+    let mut h_vec:Vec<u64> = Vec::new();
+    let mut w_vec:Vec<u64> = Vec::new();
+    for i in 0..h {
+        h_vec.push(a[i].iter().fold(0, |sum, e| sum + e));
+    }
+
+    for j in 0..w {
+        w_vec.push(a.iter().fold(0, |sum, e| sum + e[j]));
+    }
+
+    for i in 0..h {
+        for j in 0..w {
+            let res = h_vec[i] + w_vec[j] - a[i][j];
+            print!("{} ", res);
+        }
+        print!("\n");
+    }
+}
