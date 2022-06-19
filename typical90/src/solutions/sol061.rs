1b8224c5285297343a54b64da94514a3fc8f662d
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2022-06-19 18:16:07 +0900

    added 55, 61, 67, 78

--- typical90/src/solutions/sol061.rs
+++ typical90/src/solutions/sol061.rs
@@ -1,10 +1,21 @@
-//
+// https://atcoder.jp/contests/typical90/tasks/typical90_bi
 
 use proconio::input;
+use std::collections::VecDeque;
 
 pub fn solve() {
     input! {
-        _: usize,
+        q: usize,
+        tx: [(usize, usize); q],
     }
-    println!("Hello!");
+    let mut deque: VecDeque<usize> = VecDeque::new();
+
+    for (t, x) in tx.iter() {
+        match *t {
+            1 => deque.push_front(*x),
+            2 => deque.push_back(*x),
+            3 => println!("{}", deque[*x - 1]),
+            _ => println!("unexpected")
+        }
+    }
 }
--- typical90/src/solutions/sol078.rs
+++ typical90/src/solutions/sol078.rs
@@ -1,10 +1,21 @@
-//
+// https://atcoder.jp/contests/typical90/tasks/typical90_bz
 
 use proconio::input;
 
 pub fn solve() {
     input! {
-        _: usize,
+        n: usize,
+        m: usize,
+        ab: [(usize, usize); m],
     }
-    println!("Hello!");
+    let mut table = vec![0; n];
+    for (a, b) in ab.iter() {
+        if *a > *b { table[*a - 1] += 1; }
+        else  { table[*b - 1] += 1; }
+    }
+    let mut ans = 0;
+    for v in table.iter() {
+        if *v == 1 { ans += 1; }
+    }
+    println!("{}", ans);
 }
